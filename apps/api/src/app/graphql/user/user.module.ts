15660774b0eb9d30d1567c290cc629af18db406f
Edmond O'Flynn <oflynned@tcd.ie>
2023-01-16 21:16:55 +0100

    chore: add graphql defs

--- apps/api/src/app/graphql/user/user.module.ts
+++ apps/api/src/app/graphql/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { UserResolver } from "./user.resolver";
 import { UserModule } from "../../domains/user/user.module";
 
 @Module({
   imports: [UserModule],
   providers: [UserResolver],
-
+  exports: [UserResolver],
 })
+export class UserGraphQLModule {}
