9fdd3c3a6d9692cd57e3b16b99ac198bc872176b
Edmond O'Flynn <oflynned@tcd.ie>
2023-01-18 17:07:54 +0100

    chore: update resolver

--- apps/api/src/app/graphql/jwt/jwt.resolver.ts
+++ apps/api/src/app/graphql/jwt/jwt.resolver.ts
@@ -1,12 +1,13 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UseGuards } from "@nestjs/common";
 import { Args, Mutation, Resolver } from "@nestjs/graphql";
 import { IAccessTokenService } from "../../domains/access-token/access-token.service";
 import { JwtResult } from "@translate-manager/graphql-types";
 import { IRefreshTokenService } from "../../domains/access-token/refresh-token.service";
 import { IAuthenticationService } from "../../domains/authentication/authentication.service";
-import { WrongPasswordException } from "../../domains/authentication/exceptions/wrong-password.exception";
-import { ApolloError } from "apollo-server-express";
 import { UserNotFoundException } from "../../domains/user/service/exceptions/user-not-found.exception";
+import { RefreshTokenEntity } from "../../domains/access-token/refresh-token.entity";
+import { RefreshTokenGuard } from "../../core/guards/refresh-token.guard";
+import { RefreshToken } from "../../core/decorators/refresh-token.decorator";
 
 @Injectable()
 @Resolver()
@@ -28,35 +29,67 @@ export class JwtResolver {
     });
 
     if (userResult.err) {
-      // TODO disguise this as user not found?
       throw new UserNotFoundException();
     }
 
-    const refreshTokenResult =
-      await this.refreshTokenService.createRefreshToken(userResult.val);
+    const [refreshTokenResult, accessTokenResult] = await Promise.all([
+      this.refreshTokenService.createRefreshToken(userResult.val),
+      this.accessTokenService.createAccessToken(userResult.val),
+    ]);
 
-    if (refreshTokenResult.err) {
+    if (refreshTokenResult.err || accessTokenResult.err) {
       return {
         __typename: "MalformedJwt",
-        message: refreshTokenResult.val.message,
-      };
-    }
-
-    const accessToken = await this.accessTokenService.createAccessToken(
-      userResult.val
-    );
-
-    if (accessToken.err) {
-      return {
-        __typename: "MalformedJwt",
-        message: accessToken.val.message,
+        message: "Could not create a valid jwt for the given user",
       };
     }
 
     return {
       __typename: "Jwt",
-      accessToken: accessToken.val,
+      accessToken: accessTokenResult.val,
       refreshToken: refreshTokenResult.val,
+    };
+  }
+
+  @Mutation("revokeRefreshToken")
+  async revokeRefreshToken(
+    @RefreshToken() refreshToken: RefreshTokenEntity
+  ): Promise<JwtResult> {
+    if (refreshToken.expiresAt < new Date()) {
+      return {
+        __typename: "ExpiredJwt",
+        expiredAt: refreshToken.expiresAt,
+      };
+    }
+
+    const result = await this.refreshTokenService.revokeToken(refreshToken);
+
+    return {
+      __typename: "RevokedJwt",
+      message: `Refresh token was revoked at ${result.val.revokedAt.toISOString()}`,
+    };
+  }
+
+  @Mutation("refreshAccessToken")
+  @UseGuards(RefreshTokenGuard)
+  async refreshAccessToken(
+    @RefreshToken() refreshToken: RefreshTokenEntity
+  ): Promise<JwtResult> {
+    const result =
+      await this.accessTokenService.createAccessTokenFromRefreshToken(
+        refreshToken
+      );
+
+    if (result.err) {
+      return {
+        __typename: "MalformedJwt",
+        message: result.val.message,
+      };
+    }
+
+    return {
+      __typename: "Jwt",
+      accessToken: result.val,
     };
   }
 }
