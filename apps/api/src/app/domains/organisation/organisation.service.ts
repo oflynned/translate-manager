afc5fbfef56a72a51cbc669c7d20a2475a948c64
Edmond O'Flynn <oflynned@tcd.ie>
2023-01-25 17:51:08 +0100

    chore: update organisation entity, repo, and service

--- apps/api/src/app/domains/organisation/organisation.service.ts
+++ apps/api/src/app/domains/organisation/organisation.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { OrganisationEntity } from "./organisation.entity";
 import { IOrganisationRepo } from "./organisation.repo";
-import { CreateOrganisationDto } from "./dto/create-organisation.dto";
-import { GetOrganisationByIdDto } from "./dto/get-organisation-by-id.dto";
+import {
+  CreateOrganisationDto,
+  DeleteOrganisationDto,
+  GetOrganisationByIdDto,
+} from "@translate-dashboard/dto";
 import { UserEntity } from "../user/repo/user.entity";
+import { Err, Ok, Result } from "ts-results";
+import { OrganisationNotFoundException } from "../../graphql/organisation/exception/organisation-not-found.exception";
+import { InvalidOrganisationException } from "../../graphql/organisation/exception/invalid-organisation.exception";
 
 export abstract class IOrganisationService {
   abstract createOrganisation(
     dto: CreateOrganisationDto,
     creator: UserEntity
-  ): Promise<OrganisationEntity>;
+  ): Promise<Result<OrganisationEntity, InvalidOrganisationException>>;
 
   abstract getOrganisationById(
     dto: GetOrganisationByIdDto
-  ): Promise<OrganisationEntity | null>;
+  ): Promise<Result<OrganisationEntity, OrganisationNotFoundException>>;
+
+  abstract deleteOrganisation(
+    dto: DeleteOrganisationDto
+  ): Promise<Result<OrganisationEntity, OrganisationNotFoundException>>;
 }
 
 @Injectable()
@@ -23,13 +33,37 @@ export class OrganisationService implements IOrganisationService {
   async createOrganisation(
     dto: CreateOrganisationDto,
     creator: UserEntity
-  ): Promise<OrganisationEntity> {
-    return this.repo.create(dto.name, creator);
+  ): Promise<Result<OrganisationEntity, InvalidOrganisationException>> {
+    const organisation = await this.repo.create(dto.name, creator);
+
+    if (!organisation) {
+      return Err(new InvalidOrganisationException());
+    }
+
+    return Ok(organisation);
   }
 
   async getOrganisationById(
     dto: GetOrganisationByIdDto
-  ): Promise<OrganisationEntity | null> {
-    return this.repo.getById(dto.id);
+  ): Promise<Result<OrganisationEntity, OrganisationNotFoundException>> {
+    const organisation = await this.repo.getById(dto.id);
+
+    if (!organisation) {
+      return Err(new OrganisationNotFoundException());
+    }
+
+    return Ok(organisation);
+  }
+
+  async deleteOrganisation(
+    dto: DeleteOrganisationDto
+  ): Promise<Result<OrganisationEntity, OrganisationNotFoundException>> {
+    const organisation = await this.repo.deleteOrganisation(dto.id);
+
+    if (!organisation) {
+      return Err(new OrganisationNotFoundException());
+    }
+
+    return Ok(organisation);
   }
 }
